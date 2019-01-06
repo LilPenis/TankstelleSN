e71b9956a5dbac1b8f388e84aa28574a1b390822
LilPenis <nikita.bradovka@gmx.ch>
2019-01-06 13:48:41 +0100

    Ned pulle

--- TankstellenPrg/TankstellenPrg/Payment.cs
+++ TankstellenPrg/TankstellenPrg/Payment.cs
@@ -12,67 +12,92 @@ namespace TankstellenPrg
 {
     public partial class Payment : Form
     {
-        int Price;
-        int MyPay;
-        public Payment(int Price)
+        public Payment()
         {
-            this.Price = Price;
             InitializeComponent();
-        }
-            
-        private void PB_Click(object sender, EventArgs e)
-        {
-            Receipt quittungg = new Receipt();
-            Close();
-            quittungg.Show();
-
-        }
-
-        private void Fü_Click(object sender, EventArgs e)
-        {
-            MyPay += 50;
-            WasBezahl.Text = Convert.ToString(MyPay);
         }
 
         private void Payment_Load(object sender, EventArgs e)
         {
-             BezahlBetrag.Text = Convert.ToString(Price);
-        }
+            using System;
+            using System.Collections.Generic;
+            using System.ComponentModel;
+            using System.Data;
+            using System.Drawing;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+            using System.Windows.Forms;
 
-        private void Ei_Click(object sender, EventArgs e)
+namespace TankstellenPrg
+    {
+        public partial class Payment : Form
         {
-            MyPay += 1;
-            WasBezahl.Text = Convert.ToString(MyPay);
-        }
+            int Price;
+            int MyPay;
+            public Payment(int Price)
+            {
+                this.Price = Price;
+                InitializeComponent();
+            }
 
-        private void Zwei_Click(object sender, EventArgs e)
-        {
-            MyPay += 2;
-            WasBezahl.Text = Convert.ToString(MyPay);
-        }
+            private void PB_Click(object sender, EventArgs e)
+            {
+                Receipt quittungg = new Receipt();
+                Close();
+                quittungg.Show();
 
-        private void Feuf_Click(object sender, EventArgs e)
-        {
-            MyPay += 5;
-            WasBezahl.Text = Convert.ToString(MyPay);
-        }
+            }
 
-        private void Zä_Click(object sender, EventArgs e)
-        {
-            MyPay += 10;
-            WasBezahl.Text = Convert.ToString(MyPay);
-        }
+            private void Fü_Click(object sender, EventArgs e)
+            {
+                MyPay += 50;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
 
-        private void Zw_Click(object sender, EventArgs e)
-        {
-            MyPay += 20;
-            WasBezahl.Text = Convert.ToString(MyPay);
-        }
+            private void Payment_Load(object sender, EventArgs e)
+            {
+                BezahlBetrag.Text = Convert.ToString(Price);
+            }
 
-        private void Hun_Click(object sender, EventArgs e)
-        {
-            MyPay += 100;
-            WasBezahl.Text = Convert.ToString(MyPay);
+            private void Ei_Click(object sender, EventArgs e)
+            {
+                MyPay += 1;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
+
+            private void Zwei_Click(object sender, EventArgs e)
+            {
+                MyPay += 2;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
+
+            private void Feuf_Click(object sender, EventArgs e)
+            {
+                MyPay += 5;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
+
+            private void Zä_Click(object sender, EventArgs e)
+            {
+                MyPay += 10;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
+
+            private void Zw_Click(object sender, EventArgs e)
+            {
+                MyPay += 20;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
+
+            private void Hun_Click(object sender, EventArgs e)
+            {
+                MyPay += 100;
+                WasBezahl.Text = Convert.ToString(MyPay);
+            }
         }
+    }
+
+}
     }
 }
